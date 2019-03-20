c5c6f1589c68b29c2729d228bfa1476075a294b1
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2019-03-20 12:31:46 +0530

    m4 a42

---
+++ Assignments/Module 7/Shopping_Cart/app/src/main/java/com/example/shopping_cart/Hero.java
@@ -0,0 +1,19 @@
+package com.example.shopping_cart;
+
+public class Data {
+        String pid, cat;
+
+        public Data(String pid, String cat) {
+            this.pid = pid;
+            this.cat = cat;
+        }
+
+        public String getPid() {
+            return pid;
+        }
+
+        public String getCat() {
+            return cat;
+        }
+    }
+
